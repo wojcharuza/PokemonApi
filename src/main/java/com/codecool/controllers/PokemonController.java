c27eda6eec519c254fab5df4b780e7d435ea7f4e
Wojciech Charuza <wojtek.charuza@gmail.com>
2019-07-10 09:59:06 +0200

    add controllers

---
+++ src/main/java/com/codecool/controllers/PokemonController.java
@@ -0,0 +1,20 @@
+package com.codecool.controllers;
+
+import com.codecool.models.Pokemon;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+public class PokemonController {
+
+    @GET
+    @Path("pokemons")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Pokemon> getPokemons() {
+        return null;
+    }
+
+}
